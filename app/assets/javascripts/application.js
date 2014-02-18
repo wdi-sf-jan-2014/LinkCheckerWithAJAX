f9bad844cee467586361a760695ae1ceced17de7
darrenwong916 <darrenwong06@gmail.com>
2014-02-17 16:08:58 -0800

    added something to javascript because tim screwed up on his application/javascript/script.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,58 +1,38 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
-
-
 var Callbacks = (function() {
 
   var createSite = function(url, data) {
-       // Make .ajax request here
+      // Make .ajax request here
   };
 
   var addNewUrlToTable = function(url, httpResponse) {
     // Actually add the url and response code to the table
   };
-  return {
-    postSuccessHandler : function(response) {
+
+  var postSuccessHandler = function(response) {
       // Call addNewUrlToTable and insert the results
       addNewUrlToTable('','');
 
-    },
+  };
 
-    postFailureHandler : function(jqXHR) {
+  var postFailureHandler  = function(jqXHR) {
       // The request failed.
-    },
+  };
 
-    onSubmitSiteClickHandler : function() {
+  var onSubmitSiteClickHandler =  function() {
       var site = $('#siteInput').val();
       
       // We have the site, now call create site
       // to make the request
-    },
+  };
+  return {
+    postSuccessHandler : postSuccessHandler,
+
+
+    postFailureHandler : postFailureHandler,
+
+    onSubmitSiteClickHandler : onSubmitSiteClickHandler,
     createSite : createSite,
-    
+
     addNewUrlToTable : addNewUrlToTable
   };  
 })();
-
-$(window).load(function() {
-  $("<label>New Site</label><br /><input type=\"text\" id=\"siteInput\"></input><button id=\"checkSite\">Check Site</button>").insertBefore("#siteTable");
-
-  // Adding the onSubmitSiteClickHandler to kick off the ajax
-  // request      
-  $('#checkSite').click(Callbacks.onSubmitSiteClickHandler);
-
-});
