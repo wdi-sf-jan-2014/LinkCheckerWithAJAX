b621efee97e515cbc457758744ed5a4ffb16b211
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2014-02-18 10:19:42 -0800

    jasmine tests

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,17 +18,23 @@
 
 var Callbacks = (function() {
 
-  var createSite = function(url, data) {
-       // Make .ajax request here
+  var createSite = function(urlFromCaller, data) {
+
+  $.ajax({
+  type: "post",
+  url: urlFromCaller,
+  data: data}).then(postSuccessHandler, postFailureHandler);
+
   };
 
   var addNewUrlToTable = function(url, httpResponse) {
+
     // Actually add the url and response code to the table
   };
 
   var postSuccessHandler = function(response) {
       // Call addNewUrlToTable and insert the results
-      addNewUrlToTable('','');
+      addNewUrlToTable(response.url,response.http_response);
 
   };
 
@@ -37,11 +43,18 @@ var Callbacks = (function() {
   };
 
   var onSubmitSiteClickHandler =  function() {
-      var site = $('#siteInput').val();
+      var site = $('#siteInput').val(); //capturing user input
       
-      // We have the site, now call create site
-      // to make the request
+      var authParam = $('meta[name=csrf-param]').attr('content');
+      var authToken = $('meta[name=csrf-token]').attr('content');
+      var data = {};
+      data[authParam] = authToken;
+      data.site = {};
+      data.site.url = site;
+
+      Callbacks.createSite('/sites.json', data );
   };
+
   return {
     postSuccessHandler : postSuccessHandler,
 
