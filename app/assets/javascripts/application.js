856a62b42cc0adfd4817cdf25d1d13034ace4952
mbetts7 <mbetts7@gmail.com>
2014-02-18 13:57:08 -0800

    added handlebars functionality the hard way

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,49 +18,65 @@
 
 var Callbacks = (function() {
 
-  var createSite = function(url, data) {
-    $.ajax({type: "POST", url: url, data: data });
-
+ var createSite = function(url, data) {
+      // Make .ajax request here
+        var authParam = $('meta[name=csrf-param]').attr('content');
+        var authToken = $('meta[name=csrf-token]').attr('content');
+        data[authParam]= authToken;
+        $.ajax({
+          type: "POST",
+          url: url,
+          data: data}).then(postSuccessHandler, postFailureHandler);
   };
 
-  var addNewUrlToTable = function(url, httpResponse) {
-    $('#siteTable').append("<tr><td><a href='" + url + "'>" + url + "</a></td>", "<td>" + httpResponse + "</td></tr>");
-  };
+var addNewUrlToTable = function(url, httpResponse) {
+   var source = "<tr> <td><a href={{url}} >{{url}}</a></td> <td> {{httpResponse}} </td> </tr>";
+   // handlebars.compile is a js function which takes a template as a string and returns it as a template function
+   var template = Handlebars.compile(source);
+   var data = {url: url, httpResponse: httpResponse};
+   $('#siteTable').append(template(data));
+   $("#siteInput").val("");
+ };
 
-  var postSuccessHandler = function(response) {
-      // Call addNewUrlToTable and insert the results
-      addNewUrlToTable('','');
+ var postSuccessHandler = function(response) {
+     // Call addNewUrlToTable and insert the results
+     Callbacks.addNewUrlToTable(response.url,response.http_response);
 
-  };
+ };
 
-  var postFailureHandler  = function(jqXHR) {
-      // The request failed.
-  };
+ var postFailureHandler  = function(jqXHR) {
+     alert("EPIC FAIL!");
+     // The request failed.
+ };
 
-  var onSubmitSiteClickHandler =  function() {
-      var site = $('#siteInput').val();
-      
-      // We have the site, now call create site
-      // to make the request
-  };
-  return {
-    postSuccessHandler : postSuccessHandler,
+ var onSubmitSiteClickHandler =  function() {
+     var site = $('#siteInput').val();
+
+     var authParam = $('meta[name=csrf-param]').attr('content');
+     var authToken = $('meta[name=csrf-token]').attr('content');
+
+     var data = {};
+     data[authParam] = authToken;
+     data.site = {url: site};
+
+     Callbacks.createSite("/sites.json", data);
+     // We have the site, now call create site
+     // to make the request
+ };
+ return {
+   postSuccessHandler : postSuccessHandler,
 
 
-    postFailureHandler : postFailureHandler,
+   postFailureHandler : postFailureHandler,
 
-    onSubmitSiteClickHandler : onSubmitSiteClickHandler,
-    createSite : createSite,
+   onSubmitSiteClickHandler : onSubmitSiteClickHandler,
+   createSite : createSite,
 
-    addNewUrlToTable : addNewUrlToTable
-  };  
+   addNewUrlToTable : addNewUrlToTable
+ };  
 })();
 
 $(window).load(function() {
-  $("<label>New Site</label><br /><input type=\"text\" id=\"siteInput\"></input><button id=\"checkSite\">Check Site</button>").insertBefore("#siteTable");
-
-  // Adding the onSubmitSiteClickHandler to kick off the ajax
-  // request      
+  $("<label>New Site</label><br /><input type=\"text\" id=\"siteInput\"></input><button id=\"checkSite\">Check Site</button>").insertBefore("#siteTable"); 
   $('#checkSite').click(Callbacks.onSubmitSiteClickHandler);
-
 });
