e712d26b5590a9a49a4b9f369a02e2424f9f4222
JSchulhof <JSchulhof@gmail.com>
2014-02-17 19:31:31 -0800

    0 failures

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,20 @@
-
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+  
 var Callbacks = (function() {
   var createSite = function(url, data) {
       // Make .ajax request here
@@ -16,13 +32,13 @@ var Callbacks = (function() {
 
   var postSuccessHandler = function(response) {
       // Call addNewUrlToTable and insert the results
-      addNewUrlToTable('','');
+      Callbacks.addNewUrlToTable(response.url,200);
 
   };
 
   var postFailureHandler  = function(jqXHR) {
       // The request failed.
-      alert("HTTP Status Code = " + jqXHR.status);
+      alert("HTTP Status Code Fail = " + jqXHR.status);
   };
 
   var onSubmitSiteClickHandler =  function() {
